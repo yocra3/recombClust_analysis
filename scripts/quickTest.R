608c577047c085a71232d47f4d1c2f326ef64ee7
Carlos Ruiz <yocra3@gmail.com>
2021-06-07 12:31:12 +0200

    Add changes

--- scripts/quickTest.R
+++ scripts/quickTest.R
@@ -10,3 +10,21 @@ library(SeqArray)
 vcf <- "gnomad.genomes.v3.1.hgdp_1kg_subset.chr21.vcf.bgz"
 gds <- "gnomad.genomes.hgdp_1kg.chr22.gds"
 seqVCF2GDS(vcf, gds)
+
+## Load chr22 from GDS using new implementation
+library(recombClust)
+library(GenomicRanges)
+library(SeqArray)
+
+load("VCF/PopulationClassification.RData")
+
+gds <- "GDS/ALL.chr22.gds"
+range <- GRanges("22:12085554-51304566")
+
+samples <- subset(samp_pop, superpop == "EUR")$name
+snpsData <- getData(gds, range, samples, 0.1)
+res <- computeRecombProbabilities_inFile(gds, range, samples)
+
+# 37363993
+# 49251154
+# 35814679
